c1678c233423c28fcba0f70d3095859d190b63a5
abed2k4 <abed2k4@gmail.com>
2023-05-07 10:23:10 +0300

    m

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 const Home = () => {
   return (
-    <div>page</div>
+  <section className="w-full flex-center flex-col" /*we want the page to take the whole screen, to be centered, and to stack over each other */>
+Hi
+    </section>
   )
 }
 
---
+++ components/Nav.jsx
@@ -0,0 +1,19 @@
+"use client"
+import Link from "next/link"
+import Image from "next/image"
+import {useState, useEffect } from 'react'
+
+
+const Nav = () => {
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href='/' className='flex gap-2 flex-center'>
+            <Image src="/images/logo.png" alt="BrandCode" width="50" height="50" className="object-contain"/>
+        </Link>
+
+    </nav>
+
+  )
+}
+
+export default Nav
